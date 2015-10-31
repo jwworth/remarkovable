96ba2677f7c7710fb5f804e8f1acccbd20f9e86b
Jake Worth <jakeworth82@gmail.com>
2015-10-31 10:58:57 -0500

    Cleanup unused variables

--- test/test_speak.rb
+++ test/test_speak.rb
@@ -3,41 +3,37 @@ require './lib/remarkovable.rb'
 
 class TestSpeak < Minitest::Test
   def test_period_ending
-    string = "we are walking."
+    string = 'we are walking.'
     mc = Remarkovable.new(string: string)
-    expected_output = 'Walking we are walking.'
 
-    assert_match /\A.*walking.*\.\z/, mc.speak
+    assert_match(/\A.*walking.*\.\z/, mc.speak)
   end
 
   def test_bang_ending
-    string = "we are walking!"
+    string = 'we are walking!'
     mc = Remarkovable.new(string: string)
-    expected_output = 'Walking we are walking.'
 
-    assert_match /\A.*walking.*!\z/, mc.speak
+    assert_match(/\A.*walking.*!\z/, mc.speak)
   end
 
   def test_question_mark_ending
-    string = "we are walking?"
+    string = 'we are walking?'
     mc = Remarkovable.new(string: string)
-    expected_output = 'Walking we are walking.'
 
-    assert_match /\A.*walking.*\?\z/, mc.speak
+    assert_match(/\A.*walking.*\?\z/, mc.speak)
   end
 
   def test_custom_key
     string = "we are walking\n\rwe are talking we are walking."
     mc = Remarkovable.new(string: string)
-    expected_output = /^We are./
 
-    assert_match expected_output, mc.speak(custom_key: 'we are')
+    assert_match(/^We are./, mc.speak(custom_key: 'we are'))
   end
 
   def test_custom_key_no_match
     string = "we are walking\n\rwe are talking we are walking."
     mc = Remarkovable.new(string: string)
 
-    assert_nil mc.speak(custom_key: 'foo bar') =~ /Foo bar/i
+    assert_nil(mc.speak(custom_key: 'foo bar') =~ /Foo bar/i)
   end
 end
